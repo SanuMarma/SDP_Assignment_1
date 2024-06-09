875db959459fcfb0bbc8bcb8e324f6d52a88b7cc
sanumarma1717 <44175498+SanuMarma@users.noreply.github.com>
2024-06-09 19:21:45 +0600

    Update answer_1.js

--- Module 6.5 Practice Day 1 - W2/answer_1.js
+++ Module 6.5 Practice Day 1 - W2/answer_1.js
@@ -1,1 +1,17 @@
+const result = 80;
 
+if (result < 0) {
+  console.log("failed");
+} else if (result >= 0 && result < 40) {
+  console.log("You got C grade");
+} else if (result >= 40 && result < 60) {
+  console.log("You got B grade");
+} else if (result >= 60 && result < 70) {
+  console.log("You got A- grade");
+} else if (result >= 70 && result < 80) {
+  console.log("You got A grade");
+} else if (result >= 80 && result < 100) {
+  console.log("You got A+ grade");
+} else {
+  console.log("invalid");
+}
